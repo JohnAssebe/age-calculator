7ffebb2a187e6f3ed6f7ec640704159088a3a018
JohnAssebe <JohnAssebe@gmail.com>
2023-12-18 14:59:59 +0300

    minor ui update :sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ function App() {
 
   return (
     <div className="relative w-full min-h-screen bg-lightGrey">
-      <div className="absolute top-[10%] left-1/2 -translate-x-1/2 px-4 md:px-16 py-6 md:py-8 mx-auto bg-white rounded-br-[60px] md:rounded-br-[120px] rounded-2xl shadow-xl max-w-fit">
+      <div className="absolute top-[10%] left-1/2 -translate-x-1/2 px-6 md:px-16 py-8 mx-auto bg-white rounded-br-[60px] md:rounded-br-[120px] rounded-2xl shadow-xl max-w-fit">
         <form onSubmit={formik.handleSubmit}>
           <div className="flex items-start justify-center gap-5 font-poppins">
             <div className="flex flex-col items-start justify-center gap-2">
@@ -113,17 +113,17 @@ function App() {
             </button>
           </div>
         </form>
-        <div className="text-4xl italic font-extrabold md:text-6xl">
-          <div className="flex items-center justify-start gap-x-2">
+        <div className="italic font-extrabold text-6xl">
+          <div className="flex items-center justify-start gap-x-2 py-1">
             <span className="text-purple duration-500">{age?.years ? age.years: "- -" }</span>
             <p>years</p>
           </div>
-          <div className="flex items-center justify-start gap-x-2">
+          <div className="flex items-center justify-start gap-x-2 py-1">
             <span className="text-purple">{age?.months ? age.months: "- -" }</span>
             <p>months</p>
           </div>
 
-          <div className="flex items-center justify-start gap-x-2">
+          <div className="flex items-center justify-start gap-x-2 py-1">
             <span className="text-purple">{age?.days ? age.days: "- -" }</span>
             <p>days</p>
           </div>
